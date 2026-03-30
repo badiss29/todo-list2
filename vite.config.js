ea205bc8808a30d57aa0e1aee07e8d997e2a95b2
badiss29 <badissbadiss29@gmail.com>
2026-03-30 11:44:11 +0100

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/todo-list2/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
